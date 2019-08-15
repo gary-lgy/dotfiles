b6ad3c97b3a110da3e0a8494e0e69b6c8f941e4d
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2019-08-15 17:18:20 +0800

    Add evince as optional dependency

--- setup_scripts/pacman_install.bash
+++ setup_scripts/pacman_install.bash
@@ -33,7 +33,7 @@ documents=(zathura zathura-pdf-poppler pandoc-bin texlive-core texlive-latexextr
 browser=(firefox-developer-edition)
 input_methods=(fcitx-im fcitx-configtool fcitx-googlepinyin)
 
-optional=(libreoffice-fresh syncthing-gtk rambox)
+optional=(libreoffice-fresh syncthing-gtk rambox evince)
 
 packages=( "${base[@]}" "${maintenance[@]}" "${monitoring[@]}" "${cli_utils[@]}" "${audio[@]}" "${gui[@]}" "${fonts[@]}" "${WM[@]}" "${terminal_emulator[@]}" "${launcher[@]}" "${gui_utils[@]}" "${misc_utils[@]}" \
   "${coding[@]}" "${documents[@]}" "${browser[@]}" "${input_methods[@]}")
