3dd15a1bdab4e944c788602b5bbf0bdb50f08052
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2019-12-10 13:39:45 +0800

    Fixes

--- polybar/.config/polybar/scripts/vpn-nordvpn-status.sh
+++ polybar/.config/polybar/scripts/vpn-nordvpn-status.sh
@@ -4,8 +4,8 @@ STATUS=$(nordvpn status | grep Status | tr -d ' ' | cut -d ':' -f2)
 
 if [ "$STATUS" = "Connected" ]; then
   echo -n " "; nordvpn status | grep -P -o '(?<=Current server: ).*?(?=.nordvpn.com)'
-elif [[ "$STATUS" =~ " onnecting" ]]; then
+elif [[ "$STATUS" =~ ^.*onnecting$ ]]; then
   echo -n "$STATUS"
 else
-    echo " NordVPN"
+  echo " NordVPN"
 fi
