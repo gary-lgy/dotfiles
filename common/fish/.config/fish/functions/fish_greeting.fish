206251a7e484dff348b7b278284298a7ace551b6
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2021-02-09 21:31:16 +0800

    Update fish greetings

--- common/fish/.config/fish/functions/fish_greeting.fish
+++ common/fish/.config/fish/functions/fish_greeting.fish
@@ -1,5 +1,11 @@
 function fish_greeting
+  # Default message
   set -l message "Learn something new every day."
+  # Read a random message from a file if it exists
+  if test -f ~/greetings.txt
+    set message (shuf -n 1 ~/greetings.txt)
+  end
+
   if command -v cowthink 2>/dev/null 1>&2; and command -v lolcat 2>/dev/null 1>&2
     cowthink $message | lolcat -t -S $i
   else
