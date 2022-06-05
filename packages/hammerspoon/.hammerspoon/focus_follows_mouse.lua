7f1154052b5e1291d1fedc058f826d945a4d9bdd
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2022-06-05 14:23:49 +0800

    Fix focus_follows_mouse for Hammerspoon console

--- packages/hammerspoon/.hammerspoon/focus_follows_mouse.lua
+++ packages/hammerspoon/.hammerspoon/focus_follows_mouse.lua
@@ -1,7 +1,29 @@
 local module = {}
 
+local libwindow = require('hs.libwindow')
+
 module.waitDuration = 0.1 -- seconds
 module.disableMod = 'fn' -- hold to disable
+
+-- use this instead of hs.window.orderedWindows() so we include the Hammerspoon console window
+-- https://github.com/Hammerspoon/hammerspoon/blob/master/extensions/window/window.lua#L173
+local function orderedWindows()
+    local orderedWinIds = libwindow._orderedwinids()
+    local allWindows = {}
+    for _, w in ipairs(hs.window.allWindows()) do
+        if w:isVisible() then
+            allWindows[w:id()] = w
+        end
+    end
+    local r = {}
+    for _, id in ipairs(orderedWinIds) do
+        local w = allWindows[id]
+        if w ~= nil then
+            table.insert(r, w)
+        end
+    end
+    return r
+end
 
 module.start = function()
     module.eventtap = hs.eventtap.new({hs.eventtap.event.types.mouseMoved}, lib.debounce(module.waitDuration, function(event)
@@ -9,7 +31,7 @@ module.start = function()
             return
         end
 
-        local windows = hs.window.orderedWindows()
+        local windows = orderedWindows()
         local windowUnderCursor = nil
         local cursorPosition = hs.geometry.new(hs.mouse.absolutePosition())
         for _, w in ipairs(windows) do
