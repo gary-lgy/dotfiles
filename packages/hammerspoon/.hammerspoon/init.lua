faf00ff640daac555206b390979801abd6fdff11
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2022-06-05 10:24:16 +0800

    Rename autoraise -> focusFollowsMouse

--- packages/hammerspoon/.hammerspoon/init.lua
+++ packages/hammerspoon/.hammerspoon/init.lua
@@ -4,7 +4,7 @@ lib = require('lib')
 local config = require('config')
 local windowChooser = require('window_chooser')
 local touchWatcher = require('touch_watcher')
-local autoraise = require('autoraise')
+local focusFollowsMouse = require('focus_follows_mouse')
 
 local function vivaldiSearch()
     local app = hs.application.get('Vivaldi')
@@ -146,6 +146,6 @@ hs.timer.doEvery(0.5, function()
 end)
 
 touchWatcher.start()
-autoraise.start()
+focusFollowsMouse.start()
 
 -- TODO: hs.layout
