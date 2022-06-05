e8fecfd39529212a3c710b1024598b0b48d4b74d
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2022-06-05 14:24:07 +0800

    Tile windows using Hammerspoon window chooser

--- packages/hammerspoon/.hammerspoon/window_chooser.lua
+++ packages/hammerspoon/.hammerspoon/window_chooser.lua
@@ -3,70 +3,6 @@ local fuzzyScore = lib.fuzzyScore
 
 local module = {}
 
--- stop using yabai for now
--- yabaiGetWindows(function(windows)
---     _chooser = showChooser(windows, yabaiFocusWindow)
--- end)
-local function yabaiParseWindows(stdOut)
-    local raw = hs.json.decode(stdOut)
-
-    local windows = {}
-    local seenIds = {}
-    for _, window in ipairs(raw) do
-        if not seenIds[window.id] and window.subrole == 'AXStandardWindow' then
-            seenIds[window.id] = true
-            local application = hs.application.find(window.pid)
-            table.insert(windows, {
-                text = window.title,
-                subText = window.app,
-                image = application and hs.image.imageFromAppBundle(application:bundleID()) or hs.image.iconFromName('NSStatusUnavailable'),
-                id = window.id,
-                frame = window.frame,
-            })
-        end
-    end
-
-    return windows
-end
-
-local function yabaiGetWindows(cb)
-    -- use hs.task instead of os.execute to prevent blocking the lua main thread
-    hs.task.new(
-        '/usr/local/bin/yabai',
-        function(exitCode, stdOut, stdErr)
-            if exitCode ~= 0 then
-                log.e('yabai returned error - [stdout] ' .. stdOut .. ' [stderr] ' .. stdErr)
-                return
-            end
-
-            local windows = yabaiParseWindows(stdOut)
-            cb(windows)
-        end,
-        { '-m', 'query', '--windows' }
-    )
-        :start()
-end
-
-local function yabaiFocusWindow(win)
-    local oldSID = hs.screen.mainScreen():id()
-    hs.task.new(
-        '/usr/local/bin/yabai',
-        function(exitCode, stdOut, stdErr)
-            if exitCode ~= 0 then
-                log.e('yabai returned error - [stdout] ' .. stdOut or '' .. ' [stderr] ' .. stdErr or '')
-                return
-            end
-
-            -- if screen changed, move cursor to center of focused window
-            local newSID = hs.screen.mainScreen():id()
-            if newSID ~= oldSID then
-                hs.mouse.absolutePosition(hs.geometry.new(win.frame).center)
-            end
-        end,
-        { '-m', 'window', tostring(win.id), '--focus' }
-    ):start()
-end
-
 -- scenario:
 -- Space 1 has window 1 (current space), Space 2 has window 2 (for the same app)
 -- If we go to space 2 and focus window 2, then go back to space 1 and invoke `getWindows`,
@@ -88,13 +24,13 @@ local function getWindows()
     end)
 end
 
-local function focusWindow(win)
+local function focusWindow(win, doWarp)
     local winObj = hs.fnutils.find(module._wf:getWindows(), function(w)
         return w:id() == win.id
     end)
 
     if winObj == nil then
-        return
+        return nil
     end
 
     local oldSID = hs.screen.mainScreen():id()
@@ -105,9 +41,44 @@ local function focusWindow(win)
     winObj:focus()
 
     local newSID = hs.screen.mainScreen():id()
-    if oldSID ~= newSID then
+    if doWarp and oldSID ~= newSID then
         lib.moveCursorToCurrentWindow()
     end
+
+    return winObj
+end
+
+local function tileWindows(first, second, targetFrame, direction)
+    local firstFrame = targetFrame
+    local secondFrame = firstFrame:copy()
+
+    if direction == hs.keycodes.map.left then
+        secondFrame.x = firstFrame.x
+        secondFrame.w = firstFrame.w / 2
+        secondFrame.h = firstFrame.h
+        firstFrame.w = firstFrame.w / 2
+        firstFrame.x = firstFrame.x + firstFrame.w
+    elseif direction == hs.keycodes.map.right then
+        secondFrame.x = firstFrame.x + firstFrame.w / 2
+        secondFrame.w = firstFrame.w / 2
+        secondFrame.h = firstFrame.h
+        firstFrame.w = firstFrame.w / 2
+    elseif direction == hs.keycodes.map.up then
+        secondFrame.y = firstFrame.y
+        secondFrame.w = firstFrame.w
+        secondFrame.h = firstFrame.h / 2
+        firstFrame.h = firstFrame.h / 2
+        firstFrame.y = firstFrame.y + firstFrame.h
+    elseif direction == hs.keycodes.map.down then
+        secondFrame.y = firstFrame.y + firstFrame.h / 2
+        secondFrame.h = firstFrame.h / 2
+        secondFrame.w = firstFrame.w
+        firstFrame.h = firstFrame.h / 2
+    end
+
+    first:setFrame(firstFrame, 0)
+    second:setFrame(secondFrame, 0)
+    second:raise()
 end
 
 -- singleton instance
@@ -116,7 +87,7 @@ module._chooser = hs.chooser.new(function(choice)
         return
     end
 
-    focusWindow(choice)
+    focusWindow(choice, true)
 end)
 
 function module.toggle()
@@ -167,22 +138,47 @@ function module.toggle()
     end
 
     local windows = getWindows()
+    local previousWindow = hs.window.frontmostWindow()
 
-    -- update preview when selection is changed
+    -- cmd+arrow[+shift]: tile previous window and selected window on the current screen (hold shift to use the frame of the previous window instead of current screen)
+    -- up/down: update preview when selection is changed
     local eventtap = hs.eventtap.new(
         {
             hs.eventtap.event.types.keyDown,
             hs.eventtap.event.types.mouseMoved
         },
         debounce(0.02, function(event)
-            if event:getType() == hs.eventtap.event.types.keyDown and
-                (event:getKeyCode() ~= hs.keycodes.map.down and event:getKeyCode() ~= hs.keycodes.map.up) then
-                -- ignore other key presses
-                return
-            end
+            if hs.eventtap.checkKeyboardModifiers()['cmd'] then
+                if event:getType() == hs.eventtap.event.types.keyDown then
+                    local rowContents = module._chooser:selectedRowContents()
+                    module._chooser:cancel()
 
-            local row = module._chooser:selectedRow()
-            previewWindow(row)
+                    if previousWindow:id() == rowContents.id then
+                        -- don't do anything if we selected the previously focused window to prevent
+                        -- spurious resizing
+                        return
+                    end
+
+                    local newWindow = hs.fnutils.find(module._wf:getWindows(), function(w)
+                        return w:id() == rowContents.id
+                    end)
+                    if newWindow == nil then
+                        return
+                    end
+
+                    local targetFrame = hs.eventtap.checkKeyboardModifiers()['shift'] and previousWindow:frame() or previousWindow:screen():frame()
+                    tileWindows(previousWindow, newWindow, targetFrame, event:getKeyCode())
+                end
+            else
+                if event:getType() == hs.eventtap.event.types.keyDown and
+                    (event:getKeyCode() ~= hs.keycodes.map.down and event:getKeyCode() ~= hs.keycodes.map.up) then
+                    -- ignore other key presses
+                    return
+                end
+
+                local row = module._chooser:selectedRow()
+                previewWindow(row)
+            end
         end))
 
     local function filterChoices(query)
