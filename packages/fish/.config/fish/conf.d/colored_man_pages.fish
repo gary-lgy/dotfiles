99213a8a0a12c3a3553db1c0cf0ab5f3ffca2a7a
Gary Liu (刘广源) <gary.liu.gy@bytedance.com>
2023-04-13 05:23:43 +0000

    Update LESS_TERMCAP variables

--- packages/fish/.config/fish/conf.d/colored_man_pages.fish
+++ packages/fish/.config/fish/conf.d/colored_man_pages.fish
@@ -1,6 +1,8 @@
+# https://unix.stackexchange.com/questions/119/colors-in-man-pages
 set -x LESS_TERMCAP_md (printf "\e[0;35m")
 set -x LESS_TERMCAP_me (printf "\e[0m")
 set -x LESS_TERMCAP_se (printf "\e[0m")
 set -x LESS_TERMCAP_so (printf "\e[01;44;33m")
 set -x LESS_TERMCAP_ue (printf "\e[0m")
 set -x LESS_TERMCAP_us (printf "\e[01;32m")
+set -x GROFF_NO_SGR 1 # required for tmux
