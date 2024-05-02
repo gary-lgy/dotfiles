dbbf90ecd248848e7aff6a8be418cd43e5375aa6
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2024-05-02 19:19:08 +0800

    fix(fzf): don't use fd if it's not available

--- packages/fish/.config/fish/conf.d/fzf.fish
+++ packages/fish/.config/fish/conf.d/fzf.fish
@@ -1,21 +1,8 @@
-# Set fzf to use fd by default
-
-if command -v fd >/dev/null 2>&1
-    set fd_bin fd
-else if command -v fdfind >/dev/null 2>&1
-    # On Debian fd is fdfind
-    set fd_bin fdfind
-else
-    echo "fd not installed!" >&2
-end
-
 if not command -v fzf >/dev/null 2>&1
     echo "fzf not installed!" >&2
+    exit
 end
 
-set -gx FZF_DEFAULT_COMMAND "$fd_bin --hidden --follow"
-set -gx FZF_CTRL_T_COMMAND "command $fd_bin --hidden --follow . \$dir"
-set -gx FZF_ALT_C_COMMAND "$fd_bin --hidden --follow --type d"
 set -gx FZF_DEFAULT_OPTS --height=40% --reverse \
   --history=$HOME/.fzf_history \
   --bind=change:top \
@@ -26,3 +13,17 @@ set -gx FZF_DEFAULT_OPTS --height=40% --reverse \
   --bind=alt-j:preview-down,alt-k:preview-up \
   --bind=alt-a:select-all
 
+# Set fzf to use fd by default
+if command -v fd >/dev/null 2>&1
+    set fd_bin fd
+else if command -v fdfind >/dev/null 2>&1
+    # On Debian fd is fdfind
+    set fd_bin fdfind
+else
+    echo "fd not installed!" >&2
+    exit
+end
+
+set -gx FZF_DEFAULT_COMMAND "$fd_bin --hidden --follow"
+set -gx FZF_CTRL_T_COMMAND "command $fd_bin --hidden --follow . \$dir"
+set -gx FZF_ALT_C_COMMAND "$fd_bin --hidden --follow --type d"
