479fbe794f8d0b8d28c7056c2f923fe7eb632f1d
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2024-10-26 21:50:54 +0800

    feat(fish): replace exa with eza

--- packages/fish/.config/fish/functions/l.fish
+++ packages/fish/.config/fish/functions/l.fish
@@ -1,4 +1,8 @@
-if command -v exa &>/dev/null
+if command -v eza &>/dev/null
+  function l --wraps=exa
+    command eza --long --all --binary --group --header --links --icons --hyperlink $argv
+  end
+else if command -v exa &>/dev/null
   function l --wraps=exa
     command exa --long --all --binary --group --header --links --color-scale $argv
   end
